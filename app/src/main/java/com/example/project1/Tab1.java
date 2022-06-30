307fc0f2641f996f98484273a7e77d8b0bebba05
euijinshin <ejshin0310@kaist.ac.kr>
2022-06-30 17:32:26 +0900

    test

--- app/src/main/java/com/example/project1/Tab1.java
+++ app/src/main/java/com/example/project1/Tab1.java
@@ -4,18 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class Tab1 extends Fragment {
 
     public Tab1() { }
 
+    private ArrayList<MainData> arrayList;
+    private MainAdapter mainAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab1, container, false);
+        View view = inflater.inflate(R.layout.tab1, container, false);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+        mainAdapter = new MainAdapter(arrayList);
+        recyclerView.setAdapter(mainAdapter);
+
+        Button btn_add = (Button) view.findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainData mainData = new MainData(R.mipmap.ic_launcher, "name", "recycler view");
+                arrayList.add(mainData);
+                mainAdapter.notifyDataSetChanged(); //새로고침
+            }
+        });
+        return view;
     }
 }
